092ea9931375367aefb33340b54af9b0c2a4297c
Kody Sanchez <Kody Sanchez>
2017-02-22 22:38:30 -0700

    Created the solution for the android app.

---
+++ HMHY/HMHY/HMHY/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// This namespace will be used to house all business logic. UI specific logic should be placed in the individual UI projects. 
+/// </summary>
+namespace HMHY
+{
+    public class Core
+    {
+        public Core()
+        {
+        }
+    }
+}
+
