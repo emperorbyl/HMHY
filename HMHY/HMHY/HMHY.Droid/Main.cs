2b064cb4e6e1e49e31cfa803fffbefa0d67c93c9
emperorbyl <benjaminprupp@gmail.com>
2017-03-29 20:39:48 -0600

    Added more SQLite db functions
    Added viewNotes, viewGoals, and addNewUser

--- HMHY/HMHY/HMHY.Droid/Main.cs
+++ HMHY/HMHY/HMHY.Droid/Main.cs
@@ -14,6 +14,7 @@ namespace HMHY.Droid
 
     public static class LoginPage
     {
+        static string connection = "hmhy-global.database.windows.net";
         public static bool getUserCredentials(string username)
         {
             bool privledge = false;
@@ -33,7 +34,7 @@ namespace HMHY.Droid
             //TODO
             //Figure out how to access the database on a phone. This will be used instead of hmhy-global.
             // Open the connection for the query.
-            SQLiteDatabase db = SQLiteDatabase.OpenDatabase("hmhy-global.database.windows.net", null, DatabaseOpenFlags.OpenReadwrite);
+            SQLiteDatabase db = SQLiteDatabase.OpenDatabase(connection, null, DatabaseOpenFlags.OpenReadwrite);
             string query = @"INSERT INTO Goal
             Values(id, @title, @description, @startTime, @endTime, reminderId)";
             // Add the values passed in to a Content Vales
@@ -60,7 +61,7 @@ namespace HMHY.Droid
         public static UserReminderInfo addNewReminder(string message, DateTime remindTime)
         {
             // Open the connection for the query.
-            SQLiteDatabase db = SQLiteDatabase.OpenDatabase("hmhy-global.database.windows.net", null, DatabaseOpenFlags.OpenReadwrite);
+            SQLiteDatabase db = SQLiteDatabase.OpenDatabase(connection, null, DatabaseOpenFlags.OpenReadwrite);
             string query = @"INSERT INTO Reminder
             Values(id, @message, @startTime)";
             // Add the values passed in to a Content Vales
@@ -83,7 +84,7 @@ namespace HMHY.Droid
             List<string> allDaGoals = new List<string>();
             bool next = false;
             // Open the connection for the query.
-            SQLiteDatabase db = SQLiteDatabase.OpenDatabase("hmhy-global.database.windows.net", null, DatabaseOpenFlags.OpenReadwrite);
+            SQLiteDatabase db = SQLiteDatabase.OpenDatabase(connection, null, DatabaseOpenFlags.OpenReadwrite);
             string query = @"SELECT gl.title, gl.goalDescription
             FROM MainUser us
             JOIN Goal gl ON gl.id = us.goalId
@@ -96,14 +97,61 @@ namespace HMHY.Droid
             // Loop throughthe results of the query to get all of the goals.
             while (!curse.IsLast && next)
             {
-                string title = curse.GetString(0);
-                string desc = curse.GetString(1);
+                string title = curse.GetString(1);
+                string desc = curse.GetString(2);
                 string goal = title + ", " + desc;
                 // Add the goals to a list to be returned.
                 allDaGoals.Add(goal);
                 next = curse.MoveToNext();
             }
             return allDaGoals;
+        }
+
+        public static List<string> viewNotes (string goalName)
+        {
+            List<string> allDaNotes = new List<string>();
+            bool next = false;
+            // Open the connection for the query.
+            SQLiteDatabase db = SQLiteDatabase.OpenDatabase(connection, null, DatabaseOpenFlags.OpenReadwrite);
+            string query = @"SELECT nt.title, nt.body
+            FROM Goal gl
+            JOIN Note nt ON nt.id = us.noteId
+            WHERE gl.title LIKE @goalName";
+            string[] values;
+            values = new string[1];
+            values[0] = goalName;
+            // Execute the query.
+            Android.Database.ICursor curse = db.RawQuery(query, values);
+            // Loop throughthe results of the query to get all of the notes.
+            while (!curse.IsLast && next)
+            {
+                string title = curse.GetString(1);
+                string body = curse.GetString(3);
+                string note = title + ", " + body;
+                // Add the notes to a list to be returned.
+                allDaNotes.Add(note);
+                next = curse.MoveToNext();
+            }
+            return allDaNotes;
+        }
+
+        public static void addNewUser(string userName, bool priv)
+        {
+            // Open the connection for the query.
+            SQLiteDatabase db = SQLiteDatabase.OpenDatabase(connection, null, DatabaseOpenFlags.OpenReadwrite);
+            string query = @"INSERT INTO Goal
+            Values(id, @userName, @privlage, goalId, noteId, calendarId, userGroupId)";
+            // Add the values passed in to a Content Vales
+            Android.Content.ContentValues values = new Android.Content.ContentValues(7);
+            values.Put("0", "0");
+            values.Put("1", userName);
+            values.Put("2", priv.ToString());
+            values.Put("3", "0");
+            values.Put("4", "0");
+            values.Put("5", "0");
+            values.Put("6", "0");
+            // Execute the query to inset into the phone database.
+            db.Insert(query, "id, userName, privelage, goalId, noteId, calendarId, userGroupId", values);
         }
     }
 }
