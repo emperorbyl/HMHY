e8b9a501ea918f75ca80c5f228e806efbad5dadd
Kody <agent.xaber@gmail.com>
2017-03-31 09:10:51 -0600

    added webservice project

---
+++ hmhyWebservice2.0/hmhyWebservice2.0/Controllers/MainUsersController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using hmhyWebservice2._0.Models;
+using hmhyWebservice2._0.DAL;
+
+namespace hmhyWebservice2._0.Controllers
+{
+    [RoutePrefix("api/MainUser")]
+    public class MainUserController : ApiController
+    {
+        private HmhyGlobalDbContext db = new HmhyGlobalDbContext();
+
+        // GET: api/MainUser
+        [Route("")]
+        public IQueryable<MainUser> GetMainUsers()
+        {
+            return db.MainUser;
+        }
+
+        // GET: api/MainUser/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(MainUser))]
+        public IHttpActionResult GetMainUser(int id)
+        {
+            MainUser mainUser = db.MainUser.Find(id);
+            if (mainUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mainUser);
+        }
+
+        // PUT: api/MainUsers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMainUser(int id, MainUser mainUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != mainUser.userId)
+            {
+                return BadRequest();
+            }
+
+           // db.Entry(mainUser).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                if (!MainUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/MainUsers
+        [ResponseType(typeof(MainUser))]
+        public IHttpActionResult PostMainUser(MainUser mainUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.MainUser.Add(mainUser);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = mainUser.userId }, mainUser);
+        }
+
+        // DELETE: api/MainUsers/5
+        [ResponseType(typeof(MainUser))]
+        public IHttpActionResult DeleteMainUser(int id)
+        {
+            MainUser mainUser = db.MainUser.Find(id);
+            if (mainUser == null)
+            {
+                return NotFound();
+            }
+
+            db.MainUser.Remove(mainUser);
+            db.SaveChanges();
+
+            return Ok(mainUser);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MainUserExists(int id)
+        {
+            return db.MainUser.Count(e => e.userId == id) > 0;
+        }
+    }
+}
---
+++ hmhyWebservice2.0/hmhyWebservice2.0/DAL/HmhyGlobalDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using hmhyWebservice2._0.Models;
+
+namespace hmhyWebservice2._0.DAL
+{
+    public class HmhyGlobalDbContext : DbContext
+    {
+        public HmhyGlobalDbContext() : base("hmhyGlobalDbEntities")
+        {
+                Database.Connection.ConnectionString = Database.Connection.ConnectionString.Replace("secret", @"!Xc-4JKK");
+        }
+
+        public DbSet<MainUser> MainUser { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
